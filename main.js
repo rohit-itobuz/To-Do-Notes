c9a503ff290388e7acb78bb4dc9b8da9860f9e74
rohit-itobuz <rohit.bharti@itobuz.com>
2023-06-23 18:40:50 +0530

    wip

--- main.js
+++ main.js
@@ -1,45 +1,91 @@
-let arr = [''];
+let inputText = document.getElementById("input-text");
+inputText = inputText.value;
+let arr = [];
+const todoButton = document.querySelector(".add-button");
+// const filterOption = document.querySelector(".filter-todo");
 
-function addtask()
-{
-    arr.unshift(document.getElementById("write").value);
-    const array_list = arr.join("<br>");
-    // document.getElementById("val").innerHTML = array_list;
-    document.getElementById("itemContainer").innerHTML+= `<div id="val" class="list-item container-fluid d-flex flex-column gap-1 flex-md-row justify-content-center">
-    <input class="item-box border py-3 " type="text" id="first-text" placeholder=>
-<h3>${array_list}</h3>
-    <div class="list-button align-self-center">
-        <button onclick="completed()" id="cut" class="btn border"><i class="fa-solid fa-check"></i></button>
-        <button onclick="deleted()" class="btn border"><i class="fa fa-trash"></i></button>
-    </div>
-</div>`
 
-//     document.body
-//   .appendChild(
-//   Object.assign(
-//     document.createElement('div'),
-//     { id:'box'}
-//   )
-// ).appendChild(
-//   Object.assign(
-//     document.createElement('button'),
-//     { innerHTML : 'button' ,
-//       id:'button-1'
+// filterOption.addEventListener("change", filterTodo);
+todoButton.addEventListener("click", addTodo);
 
-//     }
-//   )
-// )
+function EnterButton(event) {
+    if (event.key === "Enter") {
+        document.getElementById("myBtn").click();
+    }
+}
+
+function addtask() {
+
+    arr.unshift(document.getElementById("input-text").value);
+    show();
+}
 
 
 
-    // for(let i=0; i< arr.length; i++)
-    // {
+function show(){
 
+    document.getElementById("itemContainer").innerHTML = "";
+    
+    
+        for (let i = 0; i < arr.length; i++) {
+            document.getElementById("itemContainer").innerHTML += `<div id="val" class="item-box w-75 bg-light container-fluid d-flex gap-1 py-2 py-lg-3 flex-md-row justify-content-center">
+                
+        <input class="container-fluid border border-0 bg-light  py-2 mx-md-2 py-lg-3" type="text" id="first-text" value="${arr[i]}">
+
+        <div class="list-button align-self-center d-flex gap-1 gap-md-2">
+            <button onclick="completed('${i}')" id="cut1" class="btn border bg-success"><i class="fa-solid fa-check"></i></button>
+            <button onclick="deleted('${i}')" id="delete1" class="btn border bg-danger"><i class="fa fa-trash"></i></button>
+
+        </div>
+
+    </div>`
+}
+//for clear the input
+    document.getElementById("input-text").value = "";
+}
+function deleted(i){
+    arr.splice(i,1);
+    show()
+    // console.log(arr);
+}
+    
+
+    //empty space invalid
+
+
+    document.getElementById("cut1").addEventListener("click", completed);
+    function completed() {
+        document.getElementById("val").style.textDecoration = "line-through";
+    }
+
+    // document.querySelector("#input-text").value = "";
+    // let curr = document.querySelectorAll("#cut1");
+    // for(let i=0; i<curr.length; i++){
+    //     curr[i].onclick = function(){
+    //         this.parentNode.remove();
+    //     }
     // }
 
-
-
+    // for (let i = 0; i < arr.length; i++) {
+    //     document.getElementById("delete1").addEventListener("click", deleted);
+    // function deleted() {
+    //     document.getElementById("val").remove(arr.splice(i,1));
+    //     // alert("hello");
+    //  }
+    // }
+        
 
 
     
-}
+
+
+
+
+
+
+
+
+// for CLEAR COMPLETED
+// document.getElementById("delete1").addEventListener("click",deleted);
+// function deleted(){
+//     document.getElementById("itemContainer").remove();
